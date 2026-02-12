63da9ba40ef23a5750f90658d792828c71d7e01e
Everton <evertton2015@hotmail.com>
2026-02-12 08:50:29 -0300

    test actions

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "i4qrxh",
    // baseUrl: 'https://automationpractice.com.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
